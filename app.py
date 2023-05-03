399f4a218ebeb133009f2ff68d2619700df556e4
Malick <malick.hamidou.boureima@gmail.com>
2023-05-03 11:37:26 -0400

    Creating database using SQLALchemy module

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 # Create an app instance using the Flask class
+
 app = Flask(__name__)
 
+# Parameters of the database
+load_dotenv()
+app.config["SECRET_KEY"] = os.getenv('APP_SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 # Render the html home page
 @app.route("/", methods=['GET', "POST"])
@@ -18,4 +37,7 @@ def index():
 
 
 # Execute the web app
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
